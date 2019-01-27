7ddc38a5667141706218d7cbc908ecfc4ace833d
Ankush Bhatia <ankush.bhatia@jungleworks.com>
2019-01-27 23:35:06 +0530

    Initial Commit

---
+++ SessionManager/SessionManager.h
@@ -0,0 +1,19 @@
+//
+//  SessionManager.h
+//  SessionManager
+//
+//  Created by Ankush Bhatia on 27/01/19.
+//  Copyright © 2019 Ankush Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SessionManager.
+FOUNDATION_EXPORT double SessionManagerVersionNumber;
+
+//! Project version string for SessionManager.
+FOUNDATION_EXPORT const unsigned char SessionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SessionManager/PublicHeader.h>
+
+
